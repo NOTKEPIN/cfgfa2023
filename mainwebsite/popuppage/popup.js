000c1409e5852b958dd2cc1c7772f5ba01cd16bc
Abwbkr-Alhag <77760239+Abwbkr-Alhag@users.noreply.github.com>
2023-09-30 15:20:07 -0700

    Popup html, css, and js, with global styles blueprint and utility file

---
+++ mainwebsite/popuppage/popup.js
@@ -0,0 +1,5 @@
+const popUpDisplay = document.getElementById('popup');
+const timeout = setTimeout(() => {
+    popUpDisplay.style.visibility = 'visible'
+    popUpDisplay.style.opacity = '1'
+}, 3000);
