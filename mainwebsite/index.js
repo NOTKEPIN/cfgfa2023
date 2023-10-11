52fbad5471ef67d296d129b66989495e3de7f576
NOTKEPIN <116948968+NOTKEPIN@users.noreply.github.com>
2023-10-10 20:49:54 -0700

    Update index.js

--- mainwebsite/index.js
+++ mainwebsite/index.js
@@ -1,1 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.getElementById("navbar-toggle");
+    const navLinks = document.querySelector(".nav-links");
 
+    menuToggle.addEventListener("click", function() {
+        navLinks.classList.toggle("show");
+    });
+});
