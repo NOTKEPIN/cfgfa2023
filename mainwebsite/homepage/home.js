77aa49e034e92a5534b5f327c6c20a74527419b9
NOTKEPIN <116948968+NOTKEPIN@users.noreply.github.com>
2023-10-10 20:50:35 -0700

    Delete mainwebsite/homepage directory

--- mainwebsite/homepage/home.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuToggle = document.getElementById("navbar-toggle");
-    const navLinks = document.querySelector(".nav-links");
-
-    menuToggle.addEventListener("click", function() {
-        navLinks.classList.toggle("show");
-    });
-});
