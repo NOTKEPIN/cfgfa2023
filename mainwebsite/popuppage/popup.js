deb3b358b510e034875ccb6c421e8bb0f7b80093
Abwbkr-Alhag <77760239+Abwbkr-Alhag@users.noreply.github.com>
2023-10-11 15:01:09 -0700

    incremental changes to popup pages, added responsiveness

--- mainwebsite/popuppage/popup.js
+++ mainwebsite/popuppage/popup.js
@@ -3,3 +3,10 @@ const timeout = setTimeout(() => {
     popUpDisplay.style.visibility = 'visible'
     popUpDisplay.style.opacity = '1'
 }, 3000);
+const exitButton = document.getElementById('exit-button');
+exitButton.addEventListener('click', function onClick() {
+    popUpDisplay.style.visibility='hidden';
+    popUpDisplay.style.opacity='0';
+    exitButton.removeEventListener('click', onClick);
+})
+
